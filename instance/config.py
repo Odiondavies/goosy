dd0def38774c3034ab36ccd66cee1140200115c9
SundayMba <sundinoh@gmail.com>
2024-09-01 18:15:48 +0100

    fix: add remaining files

---
+++ instance/config.py
@@ -0,0 +1,7 @@
+ADMIN_EMAIL = 'joachim@gmail.com'
+APP_NAME = 'grooveApp'
+DEBUG = True
+ENV = 'development'
+SECRET_KEY = 'iam/number4&'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root@localhost/groovekitchendb'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
---
+++ setup.py
@@ -0,0 +1,5 @@
+from groovekitchen import app
+
+if __name__ == '__main__':
+    app.run()
+
